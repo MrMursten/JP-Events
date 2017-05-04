4d1333ed19532ea6f89ac745978a8d8255a36cbb
MrMursten <andersbc@gmail.com>
2017-05-04 22:03:02 +0200

    First

---
+++ JP - Events/Event.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JP___Events
+{
+
+    public class Rootobject
+    {
+        public bool success { get; set; }
+        public string message { get; set; }
+        public int last_change { get; set; }
+        public Event[] events { get; set; }
+    }
+
+    public class Event
+    {
+        public string category { get; set; }
+        public string category_id { get; set; }
+        public Datelist[] datelist { get; set; }
+        public string description { get; set; }
+        public string description_english { get; set; }
+        public string description_german { get; set; }
+        public string eventgroup { get; set; }
+        public string eventid { get; set; }
+        public string family_friendly { get; set; }
+        public int last_updated { get; set; }
+        public string location_address { get; set; }
+        public string location_city { get; set; }
+        public string location_id { get; set; }
+        public object location_latitude { get; set; }
+        public object location_longitude { get; set; }
+        public string location_name { get; set; }
+        public string location_postcode { get; set; }
+        public string organizer_email { get; set; }
+        public string organizer_name { get; set; }
+        public string organizer_phone { get; set; }
+        public string picture_name { get; set; }
+        public string price { get; set; }
+        public string subcategory { get; set; }
+        public string subcategory_id { get; set; }
+        public string subtitle { get; set; }
+        public string subtitle_english { get; set; }
+        public string subtitle_german { get; set; }
+        public string tags { get; set; }
+        public string tickets_url { get; set; }
+        public string title { get; set; }
+        public string title_english { get; set; }
+        public string title_german { get; set; }
+        public string url { get; set; }
+        public string user_id { get; set; }
+        public string video_url { get; set; }
+
+        public string dateStart {
+            get
+            {
+                DateTime start = new DateTime(this.datelist[0].start * 1000);
+                return start.ToString();
+            }
+        }
+    }
+
+    public class Datelist
+    {
+        public int start { get; set; }
+        public int end { get; set; }
+    }
+
+    public class ComboboxItem
+    {
+        public string Text { get; set; }
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+
+}
---
+++ JP - Events/MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JP___Events
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Event[] events;
+        public string selectedEventPeriod;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            string jsonString = new WebClient().DownloadString("http://events.makeable.dk/api/getEvents");
+
+            JsonSerializer serializer = new JsonSerializer();
+
+            Rootobject root = JsonConvert.DeserializeObject<Rootobject>(jsonString);
+
+            events = root.events;
+
+            listBox.ItemsSource = events;
+            listBox.DisplayMemberPath = "title";
+
+            ComboboxItem s1 = new ComboboxItem();
+            s1.Text = "Last Added";
+            s1.Value = "Last";
+
+            ComboboxItem s2 = new ComboboxItem();
+            s2.Text = "Start Time";
+            s2.Value = "Time";
+
+            ComboboxItem s3 = new ComboboxItem();
+            s3.Text = "Alphabetically";
+            s3.Value = "Title";
+
+            cbSort.Items.Add(s1);
+            cbSort.Items.Add(s2);
+            cbSort.Items.Add(s3);
+
+            ComboboxItem c1 = new ComboboxItem();
+            c1.Text = "Any";
+            c1.Value = "Any";
+
+            ComboboxItem c2 = new ComboboxItem();
+            c2.Text = "Music";
+            c2.Value = "Musik";
+
+            ComboboxItem c3 = new ComboboxItem();
+            c3.Text = "City Life";
+            c3.Value = "Byliv";
+
+            ComboboxItem c4 = new ComboboxItem();
+            c4.Text = "Exhibition";
+            c4.Value = "Udstilling";
+
+            cbCategory.Items.Add(c1);
+            cbCategory.Items.Add(c2);
+            cbCategory.Items.Add(c3);
+            cbCategory.Items.Add(c4);
+
+            cbSort.SelectedIndex = 0;
+            cbCategory.SelectedIndex = 0;
+        }
+
+        public void updateListBox()
+        {
+            Event[] newEvents = events;
+
+            if (cbCategory.SelectedItem != null &&  cbSort.SelectedItem != null)
+            {
+                //Sort
+                string sortString = (cbSort.SelectedItem as ComboboxItem).Value;
+                if (sortString == "Time")
+                {
+                    newEvents = newEvents.OrderBy(x => x.datelist.Length != 0 ? x.datelist[0].start : long.MinValue).ToArray();
+                }
+                else if (sortString == "Title")
+                {
+                    newEvents = newEvents.OrderBy(x => x.title).ToArray();
+                }
+
+                //Filter
+                string filterString = (cbCategory.SelectedItem as ComboboxItem).Value;
+                if (filterString == "Musik")
+                {
+                    List<Event> listEvents = new List<Event>();
+                    foreach (Event e in newEvents)
+                    {
+                        if (e.category == "Musik")
+                        {
+                            listEvents.Add(e);
+                        }
+                    }
+                    newEvents = listEvents.ToArray<Event>();
+                }
+                else if (filterString == "Byliv")
+                {
+                    List<Event> listEvents = new List<Event>();
+                    foreach (Event e in newEvents)
+                    {
+                        if (e.category == "Byliv")
+                        {
+                            listEvents.Add(e);
+                        }
+                    }
+                    newEvents = listEvents.ToArray<Event>();
+                }
+                else if (filterString == "Udstilling")
+                {
+                    List<Event> listEvents = new List<Event>();
+                    foreach (Event e in newEvents)
+                    {
+                        if (e.category == "Udstilling")
+                        {
+                            listEvents.Add(e);
+                        }
+                    }
+                    newEvents = listEvents.ToArray<Event>();
+                }
+
+                listBox.ItemsSource = newEvents;
+                listBox.DisplayMemberPath = "title";
+            }
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listBox.SelectedItem != null)
+            {
+                Event ev = (Event)listBox.SelectedItem;
+                long startMilis = ev.datelist.Length != 0 ? (ev.datelist[0].start) : 0;
+                long endMilis = ev.datelist.Length != 0 ? (ev.datelist[0].end) : 0;
+
+                String start = startMilis > 0 ? new DateTime(startMilis * 10000).ToString() : "";
+                String end = endMilis > 0 ? new DateTime(endMilis * 10000).ToString() : "";
+                selectedEventPeriod = start + " - " + end;
+                txtTime.Text = selectedEventPeriod;
+            }
+        }
+
+        private void cbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            updateListBox();
+        }
+
+        private void cbCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            updateListBox();
+        }
+    }
+}
